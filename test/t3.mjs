b03d69c4395f649c7323e4193236f2e873c8cdef
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2025-01-21 22:19:44 +0100

    Committing WIP before bed

--- test/t3.mjs
+++ test/t3.mjs
@@ -1,39 +1,15 @@
-
 //import * as D from '../lib/dimensions.mjs';
+import { Abbrevations as U } from '../lib/units.mjs';
+//import { Abbrevations as P } from '../lib/prefixes.mjs';
 
-import * as U from '../lib/units.mjs';
-import * as P from '../lib/prefixes.mjs';
-
-console.log(U.Abbrevations.kg);
-
-
-
-
-
-
-
-
-class Unit {
-
-/*
-
-	name
-	symbol					//Single string or list of strings
-	default_reference		//t for time or N for count
-	dimension
-	purpose
-	reference_unit
+console.log(U.kg.format_value(1, U.kg.default_prefix));		//	1 kilogram
+console.log(U.kg.format_value(3, U.kg.default_prefix));		//	3 kilograms
+console.log(U.kg.format_value(0, U.kg.default_prefix));		//	0 kilograms
+console.log(U.kg.format_value(-1, U.kg.default_prefix));	//	-1 kilograms
+console.log(U.s.format_value(3, U.s.default_prefix));		//	3 seconds
+console.log(U.C.format_value(27, U.C.default_prefix));		//	27 degrees celsius
+console.log(U.F.format_value(1, U.F.default_prefix));		//	1 degree fahrenheit
 
 
-	//runtime requirements
-	//preferred notation
-	//formatting
-
-
-
-*/
-
-
-}
 
 
