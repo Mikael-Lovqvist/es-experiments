dd17d8a9d08c978d3c46b7a0ac52aeadb0f987ab
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2025-01-21 21:18:48 +0100

    Added core-types, units, dimensions

---
+++ interactive-experiments.sh
@@ -0,0 +1,2 @@
+#NOTE - will only work on Mikael's computer for now
+hot_filewatch test/ lib/ 'clear; node $HOT_FILE'
---
+++ test/t3.mjs
@@ -0,0 +1,39 @@
+
+//import * as D from '../lib/dimensions.mjs';
+
+import * as U from '../lib/units.mjs';
+import * as P from '../lib/prefixes.mjs';
+
+console.log(U.Abbrevations.kg);
+
+
+
+
+
+
+
+
+class Unit {
+
+/*
+
+	name
+	symbol					//Single string or list of strings
+	default_reference		//t for time or N for count
+	dimension
+	purpose
+	reference_unit
+
+
+	//runtime requirements
+	//preferred notation
+	//formatting
+
+
+
+*/
+
+
+}
+
+
