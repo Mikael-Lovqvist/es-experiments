a65113ebfeb59c37011e903dce0a8832ac193c87
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2025-01-22 21:16:55 +0100

    Added resolver

--- test/t4.mjs
+++ test/t4.mjs
@@ -1,12 +1,12 @@
 import * as DU from '../lib/derived_units.mjs';
-import { simple_parser } from '../lib/simple_parser.mjs';
+import { Simple_Parser } from '../lib/simple_parser.mjs';
 
 
 //This only works in node, just for testing
 import { inspect } from 'util';
 
 
-console.log(inspect(simple_parser('kg·m²·s⁻²'), { depth: null, colors: true }));
+console.log(inspect(Simple_Parser('kg·m²·s⁻²'), { depth: null, colors: true }));
 
 /*
 Product {
---
+++ test/t5.mjs
@@ -0,0 +1,55 @@
+import { Abbrevations as D } from '../lib/dimensions.mjs';
+import { Abbrevations as U } from '../lib/units.mjs';
+
+import { Simple_Parser } from '../lib/simple_parser.mjs';
+import { Resolver } from '../lib/resolver.mjs';
+
+
+//This only works in node, just for testing
+import { inspect } from 'util';
+
+
+const resolved_expr = (new Resolver({ ...D, ...U })).resolve(Simple_Parser('kg·m²·s⁻²'));
+console.log(inspect(resolved_expr, { depth: null, colors: true }));
+
+/*
+Product {
+  operands: [
+    Unit {
+      dimension: undefined,
+      name: Countable { singular: 'Gram', plural: 'Grams' },
+      abbrevation: 'g',
+      default_prefix: Prefix {
+        name: 'Kilo',
+        abbrevation: 'k',
+        scaling_factor: 1000,
+        identity_override: undefined
+      },
+      identity_override: null,
+      sub_unit: undefined
+    },
+    Power {
+      base: Unit {
+        dimension: undefined,
+        name: Countable { singular: 'Meter', plural: 'Meters' },
+        abbrevation: 'm',
+        default_prefix: null,
+        identity_override: null,
+        sub_unit: undefined
+      },
+      exponent: Constant { value: 2 }
+    },
+    Power {
+      base: Unit {
+        dimension: undefined,
+        name: Countable { singular: 'Second', plural: 'Seconds' },
+        abbrevation: 's',
+        default_prefix: null,
+        identity_override: null,
+        sub_unit: undefined
+      },
+      exponent: Constant { value: -2 }
+    }
+  ]
+}
+*/
