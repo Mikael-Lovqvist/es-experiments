029a7a49fe047727674697d7cfd69ade1a02d94d
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2025-02-11 01:17:50 +0100

    Worked a bit on parser/resolver

--- test/t14.mjs
+++ test/t14.mjs
@@ -1,3 +1,14 @@
+//TODO: This should be rewritten a bit. We need to add a reducing ruleset that can boil down expressions.
+/*
+
+I am thinking that perhaps I should define some nice interfaces for the context to make it easier to work with,
+perhaps a pending flat sequence for the context, that way we could have a rule system that could for instance
+first try to reduce things already on the stack, then it will see if there are things pending in the flat sequence
+and work through that until there is nothing left to do and hopefully at that time there is only one thing on the
+stack that can be returned to the parent expression.
+
+*/
+
 // Separate tokens as symbols and patterns
 
 import { inspect } from 'util';
@@ -6,6 +17,9 @@ import * as P from 'ml-es-experiments/parser.mjs';
 import * as PRL from 'ml-es-experiments/parser_rule_list.mjs';
 import * as PR from 'ml-es-experiments/parser_rule.mjs';
 import * as PA from 'ml-es-experiments/parser_action.mjs';
+import * as PM from 'ml-es-experiments/parser_match.mjs';
+
+
 import { Character_Mapping } from 'ml-es-experiments/string_utils.mjs';
 
 
@@ -60,6 +74,103 @@ const pdef = new P.Rule_Based_Parser({
 //const test_string = '(230 sqrt(2))V / (10 MΩ + 10Ω) + 5²';
 const test_string = '(P₁ - P₀)f + P₀';
 
-const res = pdef.parse('expression', test_string);
-console.log(inspect(res, { depth: null, colors: true }));
+const res = new PM.Sequence_Match(TS.EXPRESSION, pdef.parse('expression', test_string));	//Because we haven't added an outer wrapper yet
+//console.log(inspect(res, { depth: null, colors: true }));
+
+
+
+
+import { Mapping_Resolver } from 'ml-es-experiments/resolver.mjs';
+
+const mr = new Mapping_Resolver();
+
+
+
+
+
+class Text {
+	constructor(value) {
+		this.value = value;
+	}
+};
+
+class Operand {};
+
+class Variable extends Operand {
+	constructor(value) {
+		super();
+		this.value = value;
+	}
+};
+
+class Index extends Operand {
+	constructor(reference, index) {
+		super();
+		this.reference = reference;
+		this.index = index;
+	}
+};
+
+
+
+class Resolver_Context {
+	constructor(parent=null, stack=[]) {
+		this.parent = parent;
+		this.stack = stack;
+	}
+
+	get top() {
+		return this.stack.at(-1);
+	}
+
+};
+
+
+class OP {
+	static SUBTRACT = Symbol('SUBTRACT');
+	static NEGATE = Symbol('NEGATE');
+};
+
+mr.add_rule(TS.HYPHEN_MINUS, (item, ctx) => {
+	if (ctx.top === undefined) {
+		ctx.stack.push(OP.NEGATE);
+	} else if (ctx.top instanceof Operand) {
+		ctx.stack.push(OP.SUBTRACT);
+	} else {
+		throw 'Hypen after what?';
+	}
+})
+
+
+mr.add_rule(TS.TEXT, (item, ctx) => {
+	ctx.stack.push(new Text(item.value));
+})
+
+mr.add_rule(TS.SUBSCRIPT, (item, ctx) => {
+	if (ctx.top instanceof Text) {
+		ctx.stack.push(new Index(new Variable(ctx.top.value), item.value));
+	} else {
+		throw 'Subscript of what?';
+	}
+
+})
+
+
+mr.add_rule(TS.EXPRESSION, (item, ctx) => {
+	const sub_context = new Resolver_Context(ctx);
+	for (const sub_item of item.sequence) {
+		resolve(sub_item, sub_context);
+	}
+})
+
+
+function resolve(item, ctx=new Resolver_Context()) {
+	return mr.resolve_by_key(item.type, item, ctx);
+}
+
+
+
+console.log(resolve(res))
+//mr.resolve_by_key(res[0].type, res[0]);
+
 
